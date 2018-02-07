0c9af6904b6f4564ce8638126db16b5856d09004
tomstafford <tom@idiolect.org.uk>
2018-02-07 19:10:39 +0000

    merge data, better plots

--- collate.py
+++ collate.py
@@ -1,69 +1,64 @@
 '''
 collate info from multiple runs of sim_expts.py
-- this version compares a single set of positive runs vs negative runs
+- calculate summary stats for shiny app
 '''
 
 import pandas as pd  
+import socket
 import os
-#os.chdir('/home/tom/Dropbox/university/toys/ddm_sims/')
-from scipy.stats import norm #for Z scores
 import matplotlib.pylab as plt
+import glob
 
-#nulldat=pd.read_csv('/home/tom/Dropbox/university/toys/ddm_sims/python_serial/output/store_nDnB_2017-10-23_15-00-47.csv',index_col='Unnamed: 0')
-#realdat=pd.read_csv('/home/tom/Dropbox/university/toys/ddm_sims/python_serial/output/store_eDnB_2017-10-23_01-54-26.csv',index_col='Unnamed: 0')
-#figlabel='vanilla'
+if 'tom' in socket.gethostname():
+    os.chdir('/home/tom/Dropbox/university/toys/ddm_sims/')
+else:
+    print("assuming running in host directory")
 
-#nulldat=pd.read_csv('/home/tom/Dropbox/university/toys/ddm_sims/python_serial/output/store_nDeB_2017-10-24_00-33-28.csv',index_col='Unnamed: 0')
-#realdat=pd.read_csv('/home/tom/Dropbox/university/toys/ddm_sims/python_serial/output/store_eDeB_2017-10-24_00-30-24.csv',index_col='Unnamed: 0')
-#figlabel='boundaryshift'
 
-nulldat=pd.read_csv('/home/tom/Dropbox/university/toys/ddm_sims/store_D0B0.csv',index_col='Unnamed: 0')
-realdat=pd.read_csv('/home/tom/Dropbox/university/toys/ddm_sims/store_D1B0.csv',index_col='Unnamed: 0')
+#rewrite so 
+#1. joins all data
+#2. generates summary statistics 
 
-figlabel='Simple drift increment between group A and group B'
 
-def calc_positives(df):
+filenames=glob.glob('for_stiching/store*.csv')
+
+
+store_df=pd.DataFrame(columns=['sample_size', 'RT_effect_size', 'Acc_effect_size', 'Drift_effect_size',
+       'RTsig', 'Acsig', 'Drsig'])
+
+for filename in filenames:
+
+    df=pd.read_csv(filename,index_col='Unnamed: 0')
+
     df['RTsig']=((df['p_value_RTs']>0) & (df['p_value_RTs']<0.05))*1 #for fascinating reasons boolean values break the sum function https://github.com/pandas-dev/pandas/issues/3752
     df['Acsig']=((df['p_value_Acc']>0) & (df['p_value_Acc']<0.05))*1 #for fascinating reasons boolean values break the sum function https://github.com/pandas-dev/pandas/issues/3752
     df['Drsig']=(df['p_value_Drift']<0.05)*1 #for fascinating reasons boolean values break the sum function https://github.com/pandas-dev/pandas/issues/3752
     
-    total_n=df.groupby('sample_size')['RTsig'].count()
-    total_sig=df.groupby('sample_size')['RTsig'].sum()
+    total_n=df.groupby(['sample_size','Drift_effect_size'])['RTsig'].count()
+    total_sig=df.groupby(['sample_size','Drift_effect_size'])['RTsig'].sum()
     
-    total_n2=df.groupby('sample_size')['Acsig'].count()
-    total_sig2=df.groupby('sample_size')['Acsig'].sum()
+    total_n2=df.groupby(['sample_size','Drift_effect_size'])['Acsig'].count()
+    total_sig2=df.groupby(['sample_size','Drift_effect_size'])['Acsig'].sum()
     
-    total_n3=df.groupby('sample_size')['Drsig'].count()
-    total_sig3=df.groupby('sample_size')['Drsig'].sum()
+    total_n3=df.groupby(['sample_size','Drift_effect_size'])['Drsig'].count()
+    total_sig3=df.groupby(['sample_size','Drift_effect_size'])['Drsig'].sum()
     
     prop_p_values_RTs = (total_sig/total_n)  # Renaming RT Proportion for a separate figure
     prop_p_values_Acc = (total_sig2/total_n2) # Renaming Accuracy Proportion for a separate figure
     prop_p_values_Drf = (total_sig3/total_n3) # Renaming Accuracy Proportion for a separate figure
+    
         
-    return pd.DataFrame(data=[prop_p_values_RTs,prop_p_values_Acc,prop_p_values_Drf])
+    
+    #pd.DataFrame(prop_p_values_RTs).reset_index().merge(pd.DataFrame(prop_p_values_Acc).reset_index()).merge(pd.DataFrame(prop_p_values_Drf).reset_index())
+    
+           
+    RT_es=df.groupby(['sample_size','Drift_effect_size'])['RT_effect_size'].mean()
+    Ac_es=df.groupby(['sample_size','Drift_effect_size'])['Acc_effect_size'].mean()
+    #Dr_es=df.groupby('sample_size')['Drift_effect_size'].mean()
+    
+    summary_df=pd.concat([prop_p_values_RTs,prop_p_values_Acc,prop_p_values_Drf,RT_es,Ac_es],axis=1).reset_index()
+    
+    store_df=pd.concat([store_df,summary_df])        
+    
 
-hits=calc_positives(realdat)
-FAs_=calc_positives(nulldat)
-
-dprimecorrection=0.01 #adding 1% is better when some FAs = 0 due to low sample size (ie doesn't 'overcorrect' for 0 FA rate)
-#add a small amount to any floor or ceiling proportions
-FAs_=FAs_+(FAs_==0)*dprimecorrection
-FAs_=FAs_-(FAs_==1)*dprimecorrection
-hits=hits+(hits==0)*dprimecorrection
-hits=hits-(hits==1)*dprimecorrection
-
-dprime=hits.apply(norm.ppf)-FAs_.apply(norm.ppf)
-
-hits.transpose().plot()
-plt.ylabel('hits')
-plt.ylim([0,1])
-plt.savefig('hits'+figlabel+'.png',bbox_inches='tight')
-
-FAs_.transpose().plot()
-plt.ylabel('FAs_')
-plt.ylim([0,1])
-plt.savefig('FAs_'+figlabel+'.png',bbox_inches='tight')
-
-dprime.transpose().plot()
-plt.ylabel('d prime')
-plt.savefig('dprime'+figlabel+'.png',bbox_inches='tight')
+store_df.to_csv('summary.csv')
