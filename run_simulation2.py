f7ce962eb848ba759dd3d5943560cfeaf11c4d04
tomstafford <tom@idiolect.org.uk>
2018-03-12 13:07:27 +0000

    tweeks

--- run_simulation2.py
+++ run_simulation2.py
@@ -11,9 +11,10 @@ Define Parameters
 
 '''experiment parameters'''
 trial_names = ['groupA','groupB'] # Specfies what each trial is running - e.g. altered number of participants
-n_subjects = [10,20,30,40,50,75,100,150,300] # n_participants in each experiment
-drift_b=[1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8]
-
+#n_subjects = [10,20,30,40,50,75,100,150,300] # n_participants in each experiment
+#drift_b=[1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8]
+n_subjects = [40,75,150] # n_participants in each experiment
+drift_b=[1,1.1]
 
 
 n_experiments = 100  # Number of simulated experiments  - make this arbitrary large for final run
@@ -28,8 +29,8 @@ trials = 40 # trial per participants
         # Inter-trial variability in v, z and t all set to 0 (No variability)
 ''' ------------------- usually only these change between runs ---- '''
 drifts=[1,1] # different drift for each group. Drift of 1->0.85% accuracy. ASSUME GROUP B BETTER IF AT ALL
-a_param=[2, 1.9] #boundary 
-suffix='SATO' 
+a_param=[2, 2] #boundary 
+suffix='NOSATO_t40' 
 ''' ------------------- ------------------------------------------ '''
 z_param=0.5 #bias 0.5 is no bias
 t_param=0.3 #non-decision time
@@ -43,7 +44,7 @@ intersubj_vars=[0.05,0.05] # [intersubj_drift_var,intersubj_boundary_var]
 
 paramfile="params_"+suffix + ".txt"
 print("saving parameters in " + suffix)
-os.system("sed -n 7,42p run_simulation2.py > " + paramfile)
+os.system("sed -n 7,44p run_simulation2.py > " + paramfile)
 
 '''
 Send of single experiments to parallel processing, getting back the p values associated with the mean participant data
