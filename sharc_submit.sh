ebfced244681c186f0f2dff1edca5eb1bc7739e5
tomstafford <tom@idiolect.org.uk>
2018-03-13 22:03:53 +0000

    tweeking params

--- sharc_submit.sh
+++ sharc_submit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #$ -pe smp 32
-#$ -l rmem=3G 
+#$ -l rmem=16G 
 #$ -P rse
 #$ -l h_rt=48:00:00
+#$ -m bea
+#$ -M tom@idiolect.org.uk
+#$ -j y
+
 
 #Load the hddm environment
 module load apps/python/conda
