d9fa74bcf3d4d4bf2a461a71509961b46a5d1b99
tomstafford <tom@idiolect.org.uk>
2018-05-04 05:50:20 +0100

    drift bug fix

--- run_simulation2.py
+++ run_simulation2.py
@@ -4,6 +4,7 @@ from functools import partial
 pool = Pool(16) #how many processors to use, 4 in the VM, 32 on the RSE Sharc cluster
 from sendemail import send_mail #for sending email
 import os #system functions
+import numpy as np #number functions
 
 '''
 Define Parameters
@@ -11,41 +12,40 @@ Define Parameters
 
 '''experiment parameters'''
 trial_names = ['groupA','groupB'] # Specfies what each trial is running - e.g. altered number of participants
-#n_subjects = [10,20,30,40,50,75,100,150,300] # n_participants in each experiment
-#drift_b=[1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8]
-n_subjects = [10,20,30,40,75,300] # n_participants in each experiment
-drift_b=[1,1.10] #smaller range of drift d's diff 1:1.25 -> cohen's d of drift = 5 .
-
-n_experiments = 1000  # Number of simulated experiments  - make this arbitrary large for final run
 n_samples = 200  #for HDDM fitting, put this to 5000 for final run
-
+#
+##
+''' ------------------- usually only these change between runs ---- '''
+n_subjects = [10] # e.g. [10,20,30,40,50,75,150] # n_participants in each experiment
+drift_b=[1,1.1] #e.g. [1,1.05,1.1,1.15,1.2] #range of drift in 2nd group
+drift_a=np.ones(len(drift_b)) # assume group a is baseline, with drift of 1 in each condition. Drift of 1->0.85% accuracy. ASSUME GROUP B BETTER IF AT ALL
+n_experiments = 4  # Number of simulated experiments  - make this arbitrary large for final run
 trials = 40 # trial per participants
-
-print("Number of simulated experiments = " + str(len(drift_b)*len(n_subjects)*n_experiments))
-
+a_param=[2, 2] #boundary  for group A group B
+suffix='SATO_t40_test' 
+''' ------------------- ------------------------------------------ '''
+##
+#
 '''HDDM parameters'''
     # HDDM Parameters:
         # v = Drift rate        # a = Boundary separation
         # t = Non-decision time # z = protent response bias
         # Inter-trial variability in v, z and t all set to 0 (No variability)
-''' ------------------- usually only these change between runs ---- '''
-drifts=[1,1] # different drift for each group. Drift of 1->0.85% accuracy. ASSUME GROUP B BETTER IF AT ALL
-a_param=[2, 2] #boundary 
-suffix='NOSATO_t40_restricted' 
-''' ------------------- ------------------------------------------ '''
 z_param=0.5 #bias 0.5 is no bias
 t_param=0.3 #non-decision time
 sv_param=0 #trial by trial drift variability
 sz_param=0
 st_param=0
-paramsA={'v':drifts[0], 'a':a_param[0], 't':t_param, 'z':z_param, 'sv':sv_param, 'sz':sz_param, 'st':st_param} # Parameters specified again
-paramsB={'v':drifts[1], 'a':a_param[1], 't':t_param, 'z':z_param, 'sv':sv_param, 'sz':sz_param, 'st':st_param} # Parameters specified again
+paramsA={'a':a_param[0], 't':t_param, 'z':z_param, 'sv':sv_param, 'sz':sz_param, 'st':st_param} # Parameters specified again
+paramsB={'a':a_param[1], 't':t_param, 'z':z_param, 'sv':sv_param, 'sz':sz_param, 'st':st_param} # Parameters specified again
 intersubj_vars=[0.05,0.05] # [intersubj_drift_var,intersubj_boundary_var]
 
 
 paramfile="params_"+suffix + ".txt"
 print("saving parameters in " + suffix)
-os.system("sed -n 7,44p run_simulation2.py > " + paramfile)
+os.system("sed -n 8,44p run_simulation2.py > " + paramfile)
+
+print("Number of simulated experiments = " + str(len(drift_b)*len(n_subjects)*n_experiments))
 
 '''
 Send of single experiments to parallel processing, getting back the p values associated with the mean participant data
@@ -55,10 +55,11 @@ start = time.time()
 block_num=0
 store_apdf = pd.DataFrame(columns=['experiment_number','sample_size','groupA_RT_mean','groupB_RT_mean','groupA_Acc_mean','groupB_Acc_mean','RT_effect_size','Acc_effect_size','Drift_effect_size','p_value_RTs','p_value_Acc','p_value_Drift','seed'])
 for ppts in n_subjects: # different sample sizes for experiments
-    for driftb in drift_b:
+    for j in range(len(drift_b)):
         start_seed = block_num*n_experiments #each batch runs with seperate start seeds
         print("Seed = " + str(start_seed))
-        paramsB['v']=driftb
+        paramsA['v']=drift_a[j]       
+        paramsB['v']=drift_b[j]
         expt_func = partial(do_experiment,ppts,paramsA,paramsB,intersubj_vars,n_samples,trial_names,trials,start_seed)
         result = pool.map(expt_func, range(n_experiments))
         result = pd.concat(result)
