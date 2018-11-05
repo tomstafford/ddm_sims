9e7ccce0a95abd574b1ffd850bfc25d698ced6c1
tomstafford <tom@idiolect.org.uk>
2018-11-05 10:53:56 +0000

    ez trial

--- run_simulation2.py
+++ run_simulation2.py
@@ -1,10 +1,15 @@
 from multiprocessing import Pool
 from sim_expts import *
+from sim_ez import do_experimentEZ,ezdiff
 from functools import partial
 pool = Pool(16) #how many processors to use, 4 in the VM, 32 on the RSE Sharc cluster
 from sendemail import send_mail #for sending email
 import os #system functions
 import numpy as np #number functions
+'''
+nb numpy > version 1.11 will break hddm
+https://groups.google.com/forum/#!topic/hddm-users/Jdwm_IoJk5U
+'''
 
 '''
 Define Parameters
@@ -16,13 +21,13 @@ n_samples = 200  #for HDDM fitting, put this to 5000 for final run
 #
 ##
 ''' ------------------- usually only these change between runs ---- '''
-n_subjects = [10] # e.g. [10,20,30,40,50,75,150] # n_participants in each experiment
+n_subjects = [10,20,30,40,50,75,150] # e.g. [10,20,30,40,50,75,150] # n_participants in each experiment
 drift_b=[1,1.1] #e.g. [1,1.05,1.1,1.15,1.2] #range of drift in 2nd group
 drift_a=np.ones(len(drift_b)) # assume group a is baseline, with drift of 1 in each condition. Drift of 1->0.85% accuracy. ASSUME GROUP B BETTER IF AT ALL
-n_experiments = 4  # Number of simulated experiments  - make this arbitrary large for final run
+n_experiments = 2000  # Number of simulated experiments  - make this arbitrary large for final run
 trials = 40 # trial per participants
 a_param=[2, 2] #boundary  for group A group B
-suffix='tempTEMP' 
+suffix='NOSATO_t40_ez' 
 ''' ------------------- ------------------------------------------ '''
 ##
 #
@@ -43,7 +48,7 @@ intersubj_vars=[0.05,0.05] # [intersubj_drift_var,intersubj_boundary_var]
 
 paramfile="params_"+suffix + ".txt"
 print("saving parameters in " + suffix)
-os.system("sed -n 8,44p run_simulation2.py > " + paramfile)
+os.system("sed -n 13,51p run_simulation2.py > " + paramfile)
 
 print("Number of simulated experiments = " + str(len(drift_b)*len(n_subjects)*n_experiments))
 
@@ -60,7 +65,7 @@ for ppts in n_subjects: # different sample sizes for experiments
         print("Seed = " + str(start_seed))
         paramsA['v']=drift_a[j]       
         paramsB['v']=drift_b[j]
-        expt_func = partial(do_experiment,ppts,paramsA,paramsB,intersubj_vars,n_samples,trial_names,trials,start_seed)
+        expt_func = partial(do_experimentEZ,ppts,paramsA,paramsB,intersubj_vars,n_samples,trial_names,trials,start_seed)
         result = pool.map(expt_func, range(n_experiments))
         result = pd.concat(result)
         store_apdf = pd.concat([store_apdf,result])
